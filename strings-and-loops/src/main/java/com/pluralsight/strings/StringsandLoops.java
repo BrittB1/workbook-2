e8ef7acd71fed239080839be315f7cab54839fcf
BrittB1 <157894600+BrittB1@users.noreply.github.com>
2024-10-08 08:22:39 -0400

    Add files via upload

---
+++ strings-and-loops/src/main/java/com/pluralsight/strings/StringsandLoops.java
@@ -0,0 +1,23 @@
+package com.pluralsight.strings;
+
+public class StringsandLoops {
+    public static void main(String[] args){
+        String s = "Hello World";
+        System.out.println(s.toLowerCase());
+
+        System.out.println("Salsa");
+        System.out.println("Milk");
+        System.out.println("Bread");
+        System.out.println("Cheese");
+        System.out.println("Hummus");
+        System.out.println("Butter");
+        System.out.println("Olives");
+        System.out.println("Pasta");
+        System.out.println("Sauce");
+        System.out.println("Chips");
+
+        String[] list = {"milk" ,"bread" ,"cheese" ,"hummus" ,"olives" ,"butter", "pasta" };
+
+
+    }
+}
